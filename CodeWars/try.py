035623a65b0fad497a0d1990e0a181d8ae08dae0
Ne0r0 <buflinskis@gmail.com>
2025-02-03 17:10:35 +0200

    Empty

--- CodeWars/try.py
+++ CodeWars/try.py
@@ -1,3 +1,7 @@
 # def greet():
 #     return f"hello world!"
 
+# DNA
+# def DNA_strand(dna):
+#     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+#     return ''.join(complement[base] for base in dna)
